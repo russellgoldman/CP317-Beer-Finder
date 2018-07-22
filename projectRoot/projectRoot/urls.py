e1de6e0a211c51f39c7d9f9ebfbe2b21a2e2a775
Abdisalan Abdi <abdisalanvp77@gmail.com>
2018-07-22 01:08:49 -0400

    configured urls.py

--- projectRoot/projectRoot/urls.py
+++ projectRoot/projectRoot/urls.py
@@ -20,4 +20,4 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
