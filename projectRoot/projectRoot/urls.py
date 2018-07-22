0710774f1d00975ed39e2635d1044595eecdd9ea
Abdisalan Abdi <abdisalanvp77@gmail.com>
2018-07-22 01:11:47 -0400

    updated urls.py

--- projectRoot/projectRoot/urls.py
+++ projectRoot/projectRoot/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
