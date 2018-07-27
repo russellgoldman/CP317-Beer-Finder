83f193aab5811afde0dd3c5edbd3ca2ddaf388a5
DavidMorenoBautista <more5580@mylaurier.ca>
2018-07-27 17:52:58 -0400

    Updated models.py - finalized database

--- projectRoot/home/admin.py
+++ projectRoot/home/admin.py
@@ -3,12 +3,15 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Brand, BodyType, Beer, Taste, ContainerType, Rating, Colour
+from .models import Brand, BodyType, Colour, ContainerType, Taste, Beer, Rating
 
 admin.site.register(Brand)
 admin.site.register(BodyType)
-admin.site.register(Beer)
-admin.site.register(Taste)
-admin.site.register(ContainerType)
-admin.site.register(Rating)
 admin.site.register(Colour)
+
+admin.site.register(ContainerType)
+admin.site.register(Taste)
+
+admin.site.register(Beer)
+
+admin.site.register(Rating)
