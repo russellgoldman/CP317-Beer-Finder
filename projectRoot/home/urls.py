24019973e3e21e79f643d8fc9f1f6c01c24c6220
Abdisalan Abdi <abdisalanvp77@gmail.com>
2018-07-26 21:33:59 -0400

    updated views and urls

--- projectRoot/home/urls.py
+++ projectRoot/home/urls.py
@@ -22,8 +22,12 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('article_page/', views.article_page, name="article_page"),
     path('top_picks/', views.top_picks, name="top_picks"),
-    path('product_page/', views.product_page, name="product_page"),
+    path('product_page/<name>', views.product_page, name="product_page"),
     path('filter_page/', views.filter_page, name="filter_page"),
     path('library_page/', views.library_page, name="library_page"),
     path('add_beer/', views.form_view, name="add_beer"),
+    path('contact/', views.contact_page, name="contact"),
+    path('about/', views.about_page, name="about"),
+    path('login/', views.login_page, name="login"),
+    path('signup/', views.signup_page, name="signup"),
 ]
